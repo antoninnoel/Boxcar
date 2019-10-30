82c6d44473a305561c2bffa6a76cd709ffb2e14a
antoninnoel <noel.antonin@gmail.com>
2019-10-30 14:37:14 +0100

    Initial Commit

---
+++ Boxcar/Boxcar.h
@@ -0,0 +1,19 @@
+//
+//  Boxcar.h
+//  Boxcar
+//
+//  Created by Antonin on 30/10/2019.
+//  Copyright © 2019 PetitBamBou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Boxcar.
+FOUNDATION_EXPORT double BoxcarVersionNumber;
+
+//! Project version string for Boxcar.
+FOUNDATION_EXPORT const unsigned char BoxcarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boxcar/PublicHeader.h>
+
+
